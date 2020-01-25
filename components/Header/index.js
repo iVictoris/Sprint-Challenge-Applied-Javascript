b53b84b605b1dae09d6a818d41e0dbf1bc123eae
Victor Tran <victor.n.tran@outlook.com>
2020-01-24 21:14:34 -0500

    create header elements, changed text, add classes, and appended child elements

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,25 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const headerDiv = document.createElement('div')
+  const dateSpan = document.createElement('span');
+  const lambdaHeader = document.createElement('h1')
+  const tempSpan = document.createElement('span');
+
+  [dateSpan, lambdaHeader, tempSpan].forEach(element => headerDiv.appendChild(element));
+
+  // add text needed here
+  dateSpan.textContent = 'SMARCH 28, 2019'
+  lambdaHeader.textContent = 'Lambda Times'
+  tempSpan.textContent = '98°'
+  
+  // change classes here
+  headerDiv.classList.add('header');
+  dateSpan.classList.add('date');
+  tempSpan.classList.add('temp');
+
+  document.querySelector('.header-container').appendChild(headerDiv)
+}
+
+Header();
